e2af31066e396057bfb527257add760f4ec55879
RavenP <ravenp.dev@gmail.com>
2023-08-04 04:16:37 +0800

    Working with strings

---
+++ strings.rb
@@ -0,0 +1,37 @@
+# String concatenation
+# sentence = 'My name is Raven'
+# p sentence
+#
+# first_name = 'Raven'
+# last_name = 'Paragas'
+#
+# puts first_name + ' ' + last_name
+#
+# String interpolation
+# puts "My first name is #{first_name} and my last name is #{last_name}"
+#
+# full_name = "#{first_name} #{last_name}"
+# puts full_name
+#
+# Methods, how to find them
+# puts ''.methods
+
+# Common methods
+# puts full_name.upcase
+# puts full_name
+# puts full_name.length
+# puts full_name.reverse
+# puts full_name.capitalize
+# puts full_name.empty?
+# puts full_name.nil?
+# puts nil.nil?
+# puts ''.empty?
+
+# Variable assignment
+sentence = 'Welcome to the jungle'
+sentence.sub!('the jungle', 'utopia')
+puts sentence
+
+# Escaping
+puts 'the new first name is \#{sentence}'
+puts "Raven asked 'Hey Nil, how are you doing?'"
