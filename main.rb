b3ec4571b738651e655c35c6e49ae49345838aa8
RavenP <ravenp.dev@gmail.com>
2023-08-04 03:35:57 +0800

    initial commit

---
+++ main.rb
@@ -0,0 +1,17 @@
+# Say hello world in multiple ways
+# Simple put string
+# Using a variable
+# Using a method
+
+puts "Hello World"
+p "Hello World"
+print "Hello World"
+
+greeting = "Hello Raven"
+puts greeting
+
+def say_hello(thing_to_say)
+  puts thing_to_say
+end
+
+say_hello "Hello World Ruby is great!"
