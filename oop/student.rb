fceee28004ee421f864a119c9297d1f51df4dd0c
RavenP <ravenp.dev@gmail.com>
2023-08-05 08:34:37 +0800

    OOP basics

---
+++ oop/student.rb
@@ -0,0 +1,31 @@
+class Student
+  attr_accessor :first_name, :last_name, :email, :username
+  attr_reader :password
+
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
+  end
+
+  # def first_name=(name)
+  #   @first_name = name
+  # end
+  #
+  # def first_name
+  #   @first_name
+  # end
+
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}"
+  end
+end
+
+raven = Student.new("raven", "paragas", "ravenp", "ravenp.dev@gmail.com", "password123")
+tin = Student.new("kristine", "paragas", "tin", "tin@gmail.com", "password123")
+
+puts raven
+puts tin
+puts tin.password
